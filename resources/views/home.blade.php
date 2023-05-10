75abecaaea3f6c0e5e6b27733ab010bddc380818
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-05-10 18:00:55 +0200

    main finito (grafica a parte)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,7 @@
 @section('page-title', 'Home Page')
 
 @section('content')
-    <h1>Benvenuto nel tuo dc comics ---main</h1>
-
-    <div class="row justify-content-center container">
+    <div class="row justify-content-center container-fluid">
         @foreach ($books as $book)
             <div class="card m-2" style="width: calc(100% /7);">
                 <img src="{{ $book['thumb'] }}" class="card-img-top" alt="...">
@@ -15,5 +13,41 @@
                 </div>
             </div>
         @endforeach
+        <div class="ms_btn text-center ">
+            <button type="button" class="btn btn-primary">LOAD MORE</button>
+        </div>
+
+        <div class="cta container-fluid mt-2 p-4 " style="background-color: #0082F9">
+
+            <div class="flex-row float-end  ps-3">
+                <img style="width: 55px;" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                <span style="color:white">DC POWER VISA</span>
+            </div>
+
+            <div class="flex-row float-end  ps-3">
+                <img style="width: 30px;" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}"
+                    alt="">
+                <span style="color:white">COMIC SHOP LOCATOR</span>
+            </div>
+
+            <div class="flex-row float-end ps-3">
+                <img style="width: 40px;" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}"
+                    alt="">
+                <span style="color:white">SUBSCRIPTIONS</span>
+            </div>
+
+            <div class="flex-row float-end  ps-3">
+                <img style="width: 40px;" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}"
+                    alt="">
+                <span style="color:white">DC MERCHANDISE</span>
+            </div>
+
+            <div class="flex-row float-end pe-4">
+                <img style="width: 40px;" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}"
+                    alt="">
+                <span style="color:white">DIGITAL COMICS</span>
+            </div>
+        </div>
+
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,9 @@
     <title>@yield('page-title')</title>
     @vite('resources/js/app.js')
 </head>
+
 <body>
-@include('partials.header')
+    @include('partials.header')
 
     <main>
         <div class="container">
@@ -16,9 +18,10 @@
         </div>
 
     </main>
-    
-@include('partials.footer')
 
-    
+    @include('partials.footer')
+
+
 </body>
+
 </html>
