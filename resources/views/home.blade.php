cbce0a7682f478905a14f7f6142da4a3cd916b93
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-05-10 17:18:10 +0200

    piccole modifiche

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,9 @@
 @section('content')
     <h1>Benvenuto nel tuo dc comics ---main</h1>
 
-    <div class="row">
+    <div class="row justify-content-center container">
         @foreach ($books as $book)
-            <div class="card" style="width: 18rem;">
+            <div class="card m-2" style="width: calc(100% /7);">
                 <img src="{{ $book['thumb'] }}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <span class="card-text">{{ $book['title'] }}</span>
